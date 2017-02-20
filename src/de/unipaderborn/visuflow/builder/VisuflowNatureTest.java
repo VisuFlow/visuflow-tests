86ce2c4e60422b853fadc26cf8d2faaca78a7e6c
nishitha <nishithas.10@gmail.com>
2017-02-20 15:58:59 +0100

    test for VisuflowNature.java

---
+++ src/de/unipaderborn/visuflow/builder/VisuflowNatureTest.java
@@ -0,0 +1,23 @@
+package de.unipaderborn.visuflow.builder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VisuflowNatureTest {
+
+	@Test
+	public void testConfigure() {
+		//fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testGetProject() {
+		VisuFlowNature vn=new VisuFlowNature();
+		Assert.assertNull(vn.getProject());
+	}
+
+
+}
