03924ea1adfabc76e1a1d54c2b161165d7eb2636
nishitha <nishithas.10@gmail.com>
2017-03-13 12:04:42 +0100

    Test for JimpleBreakpoint

---
+++ src/de/unipaderborn/visuflow/debug/ui/JimpleBreakpointTarget.java
@@ -0,0 +1,36 @@
+package de.unipaderborn.visuflow.debug.ui;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchPart;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JimpleBreakpointTarget  {
+	String id="targetID";
+	JimpleBreakpointToggleTargetFactory j=new JimpleBreakpointToggleTargetFactory();
+	IWorkbenchPart testpart;
+	ISelection testselection;
+	@Test
+	public void testGetToggleTargets() {
+		Assert.assertNotNull(j.getToggleTargets(testpart,testselection));
+	}
+
+	@Test
+	public void testGetDefaultToggleTarget() {
+		Assert.assertNotNull(j.getDefaultToggleTarget(testpart,testselection));
+	}
+
+	@Test
+	public void testGetToggleTargetName() {
+		
+		Assert.assertNotNull(j.getToggleTargetName(id));
+	}
+
+	@Test
+	public void testGetToggleTargetDescription() {
+		Assert.assertNull(j.getToggleTargetDescription(id));
+	}
+
+	
+
+}
